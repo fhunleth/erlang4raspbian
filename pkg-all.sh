537ce755391d81ae3bc9b0e78249f058807627a4
Frank Hunleth <fhunleth@troodon-software.com>
2017-05-31 12:11:51 -0400

    Fix install path for .deb files

--- pkg-all.sh
+++ pkg-all.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Clean up any old .debs
+rm -f *.deb
+
 # Running:
 #  1. Make sure that the current user has passwordless sudo access
 #  2. Run `nohup ./pkg-all.sh &` so that you can log off
@@ -10,6 +13,6 @@ set -e
 ./pkg-erlang.sh
 
 # Building Elixir requires Erlang to be installed, so install it now.
-sudo dpkg -i ./otp/*.deb
+sudo dpkg -i rpi-erlang*.deb
 ./pkg-elixir.sh
 
